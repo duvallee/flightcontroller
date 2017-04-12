f843305ff4cd3a1d5a9381b1e9b2bb5fb49112af
duvallee.lee <duvallee@asat.kr>
2017-04-12 18:14:56 +0900

    fixed does not sync path of source

--- gdb_cleanflight.sh
+++ gdb_cleanflight.sh
@@ -1,1 +1,4 @@
-./toolchain/linux/gcc-arm-none-eabi-6-2017-q1-update/bin/arm-none-eabi-gdb ./output/cleanflight/cleanflight_STM32F3DISCOVERY.elf
+pushd .
+cd cleanflight
+../toolchain/linux/gcc-arm-none-eabi-6-2017-q1-update/bin/arm-none-eabi-gdb ./obj/main/cleanflight_STM32F3DISCOVERY.elf
+popd
