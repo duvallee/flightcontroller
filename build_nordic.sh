d1c998f37391646c44491b4c49d25991cf426f1a
duvallee <duvallee@nate.com>
2017-04-14 21:27:35 +0900

    fixed wrong script for nordic's peripheral

--- build_nordic.sh
+++ build_nordic.sh
@@ -1,2 +1,2 @@
-./build.sh build=nordic target=s130_central toolchain=6.3.1
+./build.sh build=nordic target=s130_peripheral toolchain=6.3.1
 ./build.sh build=nordic target=s130_central toolchain=6.3.1
